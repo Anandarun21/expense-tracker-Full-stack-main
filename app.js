8194ea48578d6b937ce721e98961c4aaf3fdfb1d
Anandarun21 <anandarun238@gmail.com>
2024-04-02 13:40:54 +0530

    Daily Expense App features

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
-const { signup, login } = require("./controllers/UserController");
+const { signup, login } = require("./controllers/user");
 const authRoutes = require("./routes/authRoutes");
 
 // Middleware to parse JSON bodies
